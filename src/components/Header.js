45af345a16aeeb2d27922afdecba68bba2a257bf
meenereem <dng4@wisc.edu>
2017-12-02 14:21:54 -0600

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Header = (props) => (
-        <div>
-            <h1>{props.title}</h1>
-            {props.subTitle && <h2>{props.subTitle}</h2>}
+        <div className="header">
+        <center>
+            <h1 className="header__title">{props.title}</h1>
+            {props.subTitle && <h2 className="header__subtitle">{props.subTitle}</h2>}
+            </center>
         </div>
     );
 
