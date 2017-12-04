40f9d955cc1e545b69ee4f42427fe77814dad9d8
kmui2 <kmui2@wisc.edu>
2017-12-03 21:43:25 -0600

    fixed css

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,20 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
         }]
     },
     devtool: 'cheap-module-eval-source-map',
